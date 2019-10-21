e53db7927a7f4843a02a7c05093925446caca545
DAFA <s185120@student.dtu.dk>
2019-10-22 01:38:52 +0200

    Startet på filhåndteringen

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ void shuffleDeck();
 void *playerOne();
 void *playerTwo();
 
-// Initializes the deck
+// Global variables
 char deck[52][20] = {
         "2 (diamond)", "3 (diamond)", "4 (diamond)", "5 (diamond)", "6 (diamond)", "7 (diamond)", "8 (diamond)",
         "9 (diamond)", "10 (diamond)", "jack (diamond)", "queen (diamond)", "king (diamond)", "ace (diamond)",
@@ -24,14 +24,28 @@ char deck[52][20] = {
 char firstHalf[26][20];
 char secondHalf[26][20];
 pthread_mutex_t revealMutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t fileMutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t revealCond = PTHREAD_COND_INITIALIZER;
 int totalCardNumber = 1;
+FILE *fPtr1, *fPtr2;
 
 int main() {
+    // Creates/empties the file representing the player's hands
+    fPtr1 = fopen("playerOneHand.txt", "w");
+    fPtr2 = fopen("playerTwoHand.txt", "w");
+    if (fPtr1 == NULL || fPtr2 == NULL) {
+        printf("[MAIN] FILE ERROR OCCURED");
+        fflush(stdout);
+        exit(-1);
+    }
+    fclose(fPtr1);
+    fclose(fPtr2);
 
+    // Shuffeling the deck
     printf("[MAIN] Shuffeling card deck\n");
     shuffleDeck();
 
+    // Splitting deck into two halves
     printf("[MAIN] Splitting deck into two halves\n");
     int index = 0;
     for(int i = 0; i < 52; i++) {
@@ -42,23 +56,19 @@ int main() {
         }
     }
 
+    // Creating threads
     pthread_t tid1, tid2;
     printf("[MAIN] Creating thread 1 (player one)\n");
     pthread_create(&tid1, NULL, playerOne, NULL);
     printf("[MAIN] Creating thread 2 (player two)\n");
     pthread_create(&tid2, NULL, playerTwo, NULL);
 
+    // Joining threads
     pthread_join(tid1, NULL);
     printf("[MAIN] Thread 1 (player one) finished\n");
     pthread_join(tid2, NULL);
     printf("[MAIN] Thread 2 (player two) finished\n");
 
-    /*
-    for(int i = 0; i < 52; i++) {
-        printf("[%d] %s\n", i, deck[i]);
-        fflush(stdout);
-    }
-    */
     return 0;
 }
 
@@ -84,18 +94,27 @@ void *playerOne() {
     for (int i = 0; i < 26; i++) {
         // Locks the mutex
         pthread_mutex_lock(&revealMutex);
-        printf("[PLAYER_ONE] Requested lock on reveal mutex\n");
+        printf("\n[PLAYER_ONE] Requested lock on reveal mutex\n");
 
-        if (totalCardNumber % 2 != 0) {
+        if (totalCardNumber % 2 == 0) {
             // If above condition is met (totalCardNumber is uneven), the below method
             // blocks this thread and unlocks the mutex, until another thread sends a signal
             pthread_cond_wait(&revealCond, &revealMutex);
         }
         totalCardNumber++;
 
-        // The card is revealed and handled
+        // The card is revealed and handled in the file representing their hand
         printf("[PLAYER_ONE] Signal received, revealing: %s\n", firstHalf[i]);
 
+        printf("[PLAYER_ONE] Dealing with file(s)\n");
+        fPtr1 = fopen("playerOneHand.txt", "a+");
+        if (fPtr1 == NULL) {
+            printf("[PLAYER_ONE] FILE ERROR OCCURED");
+            fflush(stdout);
+            exit(-1);
+        }
+        fprintf(fPtr1, "%s\n", firstHalf[i]);
+        fclose(fPtr1);
         //TODO: Lav filehåndtering og regler her
 
         // A signal to a potentially waiting player two is sent
@@ -117,13 +136,23 @@ void *playerTwo() {
         pthread_mutex_lock(&revealMutex);
         printf("[PLAYER_TWO] Requested lock on reveal mutex\n");
 
-        if (totalCardNumber % 2 == 0) {
+        if (totalCardNumber % 2 != 0) {
             pthread_cond_wait(&revealCond, &revealMutex);
         }
         totalCardNumber++;
 
         printf("[PLAYER_TWO] Signal received, revealing: %s\n", secondHalf[i]);
+        sleep(1);
 
+        printf("[PLAYER_TWO] Dealing with file(s)\n");
+        fPtr2 = fopen("playerTwoHand.txt", "a+");
+        if (fPtr2 == NULL) {
+            printf("[PLAYER_TWO] FILE ERROR OCCURED");
+            fflush(stdout);
+            exit(-1);
+        }
+        fprintf(fPtr2, "%s\n", secondHalf[i]);
+        fclose(fPtr2);
         //TODO: Lav filehåndtering og regler her
 
         printf("[PLAYER_TWO] Signaling (unblocking) player one\n");
